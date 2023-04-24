27877b1682010685484ff1d45a43010ee1e92289
Diego Cuzco <130410185+ivancuzco@users.noreply.github.com>
2023-04-24 12:47:11 -0500

    Add files via upload

---
+++ File directorio diferente.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 19 14:20:55 2023
+
+@author: user
+"""
+
+file=open("C:/Users/user/OneDrive - Escuela Politécnica Nacional/Semestre 2022B/Python/curso/devices.txt","r")
+for item in file:
+    print(item)
+file.close()
