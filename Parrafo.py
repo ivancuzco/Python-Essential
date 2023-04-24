4e6b264086a0d746630cf2927210245a37b1bfb4
Diego Cuzco <130410185+ivancuzco@users.noreply.github.com>
2023-04-24 12:40:00 -0500

    Add files via upload

---
+++ Parrafo.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 18 13:38:47 2023
+
+@author: user
+"""
+
+x=input("Ingrese su nombre: ")
+y=input("Ingrese su apellido: ")
+z=input("Ingrese su ubicación: ")
+a=input("Ingrese su edad: ")
+print("Hola, mi nombre es "+x+" "+y+", tengo "+a+" años y vivo en "+z)
---
+++ ingreso de datos.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 13 15:16:10 2023
+
+@author: ENTERPRISE
+"""
+
+print("ingrese un dato")
+a=input()
+print("ingrese un dato")
+b=input()
